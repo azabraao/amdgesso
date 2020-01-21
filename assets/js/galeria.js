6f302a44f33a5863e8ab7d4d71d3652ca4a4b945
azabraao <az.abraao@gmail.com>
2020-01-21 13:19:30 -0300

    Add instagram-like image gallery

--- assets/js/galeria.js
+++ assets/js/galeria.js
@@ -1,32 +1,38 @@
-$(document).ready(function() {
-  let folder = document.querySelector('[data-scriptGaleria]').dataset.scriptgaleria;
-  loadImages(`galeria/${folder}/`);
+$(document).ready(function () {
+  $('.jsGalleryBg').on('click', function () {
+    $('.jsGallery').removeClass('is-active');
+  });
+  $('.jsCallGallery').on('click', function () {
+    $('.jsGallery').addClass('is-active');
+    let imgIndex = $(this).attr('data-img-index');
+    $('.cycle-slideshow').cycle('goto', imgIndex);
+  });
 })
 
 function loadImages(folder) {
-  console.log("folder: "+folder)
+  console.log("folder: " + folder)
   $.ajax({
-    url : folder,
+    url: folder,
     success: function (data) {
       $(data).find("a").attr("href", function (i, val) {
-        if( val.toLowerCase().match(/\.(jpe?g|png|gif)$/) ) { 
-          console.log('val:'+val);
-          let image = "<img src='"+val+"'>";
+        if (val.toLowerCase().match(/\.(jpe?g|png|gif)$/)) {
+          console.log('val:' + val);
+          let image = "<img src='" + val + "'>";
           addImageToGallery(image);
-        } 
+        }
       });
     }
   });
 }
 
 function addImageToGallery(img) {
-  $('.cycle-slideshow').cycle('add',img);
+  $('.cycle-slideshow').cycle('add', img);
 }
 
 function removeImageFromGallery(img) {
-  $('.cycle-slideshow').cycle('add',slideIndex - 1);
+  $('.cycle-slideshow').cycle('add', slideIndex - 1);
 
-  if (slideIndex !== 0){
-      slideIndex--;
+  if (slideIndex !== 0) {
+    slideIndex--;
   }
 }
