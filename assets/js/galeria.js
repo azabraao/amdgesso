e481f85c9f4ffa81bd47570b96cae82f2c57043a
azabraao <az.abraao@gmail.com>
2021-01-17 15:53:01 -0300

    fix carousel gallery

--- assets/js/galeria.js
+++ assets/js/galeria.js
@@ -1,36 +1,51 @@
 $(document).ready(function () {
-  $('.jsGalleryBg').on('click', function () {
-    $('.jsGallery').removeClass('is-active');
+  $(".jsGalleryBg").on("click", function () {
+    $(".jsGallery").removeClass("is-active");
   });
-  $('.jsCallGallery').on('click', function () {
-    $('.jsGallery').addClass('is-active');
-    let imgIndex = $(this).attr('data-img-index');
-    $('.cycle-slideshow').cycle('goto', imgIndex);
+
+  $(".jsCallGallery").on("click", function () {
+    
+    $(".jsGallery").addClass("is-active");
+    let allImages = document.querySelectorAll(".cycle-slideshow img");
+    let arrImages = Array.from(allImages);
+    let clickedImgSrc = $(this).attr("src");
+
+    arrImages.find((item, index) => {
+      let itemSrc = $(item).attr("src");
+      let foundImageInSlide = itemSrc === clickedImgSrc;
+      
+      if (foundImageInSlide) {
+        $(".cycle-slideshow").cycle("goto", index == 0 ? 0 : index - 1);
+        return;
+      }
+    });
   });
-})
+});
 
 function loadImages(folder) {
-  console.log("folder: " + folder)
+  console.log("folder: " + folder);
   $.ajax({
     url: folder,
     success: function (data) {
-      $(data).find("a").attr("href", function (i, val) {
-        if (val.toLowerCase().match(/\.(jpe?g|png|gif)$/)) {
-          console.log('val:' + val);
-          let image = "<img src='" + val + "'>";
-          addImageToGallery(image);
-        }
-      });
-    }
+      $(data)
+        .find("a")
+        .attr("href", function (i, val) {
+          if (val.toLowerCase().match(/\.(jpe?g|png|gif)$/)) {
+            console.log("val:" + val);
+            let image = "<img src='" + val + "'>";
+            addImageToGallery(image);
+          }
+        });
+    },
   });
 }
 
 function addImageToGallery(img) {
-  $('.cycle-slideshow').cycle('add', img);
+  $(".cycle-slideshow").cycle("add", img);
 }
 
 function removeImageFromGallery(img) {
-  $('.cycle-slideshow').cycle('add', slideIndex - 1);
+  $(".cycle-slideshow").cycle("add", slideIndex - 1);
 
   if (slideIndex !== 0) {
     slideIndex--;
